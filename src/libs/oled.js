80d8d2af90fe32b9e046ceb83800aef9b846f607
bikekoala <carryon.evan@gmail.com>
2020-12-21 19:14:12 +0800

    first commit

---
+++ src/libs/oled.js
@@ -0,0 +1,655 @@
+var i2c = require('i2c');
+
+var Oled = function(opts) {
+
+  if (opts == null) {
+    opts = {}
+  }
+
+  this.HEIGHT = opts.height || 32;
+  this.WIDTH = opts.width || 128;
+  this.ADDRESS = opts.address || 0x3C;
+  this.DEVICE = opts.device || '/dev/i2c-1';
+  this.MICROVIEW = opts.microview || false;
+  this.DATA_SIZE = opts.datasize || 16;
+
+  // create command buffers
+  this.DISPLAY_OFF = 0xAE;
+  this.DISPLAY_ON = 0xAF;
+  this.SET_DISPLAY_CLOCK_DIV = 0xD5;
+  this.SET_MULTIPLEX = 0xA8;
+  this.SET_DISPLAY_OFFSET = 0xD3;
+  this.SET_START_LINE = 0x00;
+  this.CHARGE_PUMP = 0x8D;
+  this.MEMORY_MODE = 0x20;
+  this.SEG_REMAP = 0xA1; // using 0xA0 will flip screen
+  this.COM_SCAN_DEC = 0xC8;
+  this.COM_SCAN_INC = 0xC0;
+  this.SET_COM_PINS = 0xDA;
+  this.SET_CONTRAST = 0x81;
+  this.SET_PRECHARGE = 0xd9;
+  this.SET_VCOM_DETECT = 0xDB;
+  this.DISPLAY_ALL_ON_RESUME = 0xA4;
+  this.NORMAL_DISPLAY = 0xA6;
+  this.COLUMN_ADDR = 0x21;
+  this.PAGE_ADDR = 0x22;
+  this.INVERT_DISPLAY = 0xA7;
+  this.ACTIVATE_SCROLL = 0x2F;
+  this.DEACTIVATE_SCROLL = 0x2E;
+  this.SET_VERTICAL_SCROLL_AREA = 0xA3;
+  this.RIGHT_HORIZONTAL_SCROLL = 0x26;
+  this.LEFT_HORIZONTAL_SCROLL = 0x27;
+  this.VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
+  this.VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
+
+  this.cursor_x = 0;
+  this.cursor_y = 0;
+
+  // new blank buffer
+  this.buffer = Buffer.alloc((this.WIDTH * this.HEIGHT) / 8);
+  this.buffer.fill(0x00);
+
+  this.dirtyBytes = [];
+
+  var config = {
+    '128x32': {
+      'multiplex': 0x1F,
+      'compins': 0x02,
+      'coloffset': 0
+    },
+    '128x64': {
+      'multiplex': 0x3F,
+      'compins': 0x12,
+      'coloffset': 0
+    },
+    '96x16': {
+      'multiplex': 0x0F,
+      'compins': 0x2,
+      'coloffset': 0,
+    },
+    // this is blended microview / normal 64 x 48, currently wip
+    '64x48': {
+      'multiplex': 0x2F,
+      'compins': 0x12,
+      'coloffset': (this.MICROVIEW) ? 32 : 0
+    },
+};
+
+  // Setup i2c
+  this.wire = new i2c(this.ADDRESS, {device: this.DEVICE});
+
+  var screenSize = this.WIDTH + 'x' + this.HEIGHT;
+  this.screenConfig = config[screenSize];
+
+  this._initialise();
+};
+
+Oled.prototype._initialise = function() {
+
+  // sequence of bytes to initialise with
+  var initSeq = [
+    this.DISPLAY_OFF,
+    this.SET_DISPLAY_CLOCK_DIV, 0x80,
+    this.SET_MULTIPLEX, this.screenConfig.multiplex, // set the last value dynamically based on screen size requirement
+    this.SET_DISPLAY_OFFSET, 0x00, // sets offset pro to 0
+    this.SET_START_LINE,
+    this.CHARGE_PUMP, 0x14, // charge pump val
+    this.MEMORY_MODE, 0x00, // 0x0 act like ks0108
+    this.SEG_REMAP, // screen orientation
+    this.COM_SCAN_DEC, // screen orientation change to INC to flip
+    this.SET_COM_PINS, this.screenConfig.compins, // com pins val sets dynamically to match each screen size requirement
+    this.SET_CONTRAST, 0x8F, // contrast val
+    this.SET_PRECHARGE, 0xF1, // precharge val
+    this.SET_VCOM_DETECT, 0x40, // vcom detect
+    this.DISPLAY_ALL_ON_RESUME,
+    this.NORMAL_DISPLAY,
+    this.DISPLAY_ON
+  ];
+
+  var i, initSeqLen = initSeq.length;
+
+  // write init seq commands
+  for (i = 0; i < initSeqLen; i ++) {
+    this._transfer('cmd', initSeq[i]);
+  }
+};
+
+// writes both commands and data buffers to this device
+Oled.prototype._transfer = function(type, val) {
+  var control;
+  if (type === 'data') {
+    control = 0x40;
+  } else if (type === 'cmd') {
+    control = 0x00;
+  } else {
+    return;
+  }
+  this.wire.writeBytes(control, [val], function () {});
+};
+
+Oled.prototype._transferData = function(val) {
+  let control = 0x40;
+  var size = this.DATA_SIZE;
+  for (var i=0; i<val.length; i+=size) {
+      var smallarray = val.slice(i,i+size);
+      this.wire.writeBytes(control, smallarray, function () {});
+  }
+};
+
+// read a byte from the oled
+Oled.prototype._readI2C = function(fn) {
+  this.wire.readByte(function(err, data) {
+    fn(data);
+  });
+};
+
+// sometimes the oled gets a bit busy with lots of bytes.
+// Read the response byte to see if this is the case
+Oled.prototype._waitUntilReady = function(callback) {
+  var done,
+      oled = this;
+
+  function tick(callback) {
+    oled._readI2C(function(byte) {
+      // read the busy byte in the response
+      busy = byte >> 7 & 1;
+      if (!busy) {
+        // if not busy, it's ready for callback
+        callback();
+      } else {
+        setTimeout(function(){tick(callback)}, 0);
+      }
+    });
+  }
+  setTimeout(function(){tick(callback)}, 0);
+};
+
+// set starting position of a text string on the oled
+Oled.prototype.setCursor = function(x, y) {
+  this.cursor_x = x;
+  this.cursor_y = y;
+};
+
+// write text to the oled
+Oled.prototype.writeString = function(font, size, string, color, wrap, linespacing, sync) {
+  var immed = (typeof sync === 'undefined') ? true : sync;
+  var wordArr = string.split(' '),
+      len = wordArr.length,
+      // start x offset at cursor pos
+      offset = this.cursor_x,
+      padding = 0, letspace = 1;
+  var leading = linespacing || 2;
+
+  // loop through words
+  for (var w = 0; w < len; w += 1) {
+    // put the word space back in
+    wordArr[w] += ' ';
+    var stringArr = wordArr[w].split(''),
+        slen = stringArr.length,
+        compare = (font.width * size * slen) + (size * (len -1));
+
+    // wrap words if necessary
+    if (wrap && len > 1 && (offset >= (this.WIDTH - compare)) ) {
+      offset = 1;
+      this.cursor_y += (font.height * size) + size + leading;
+      this.setCursor(offset, this.cursor_y);
+    }
+
+    // loop through the array of each char to draw
+    for (var i = 0; i < slen; i += 1) {
+      // look up the position of the char, pull out the buffer slice
+      var charBuf = this._findCharBuf(font, stringArr[i]);
+      // read the bits in the bytes that make up the char
+      var charBytes = this._readCharBytes(charBuf);
+      // draw the entire character
+      this._drawChar(font, charBytes, size, false);
+
+      // calc new x position for the next char, add a touch of padding too if it's a non space char
+      padding = (stringArr[i] === ' ') ? 0 : size + letspace;
+      offset += (font.width * size) + padding;
+
+      // wrap letters if necessary
+      if (wrap && (offset >= (this.WIDTH - font.width - letspace))) {
+        offset = 1;
+        this.cursor_y += (font.height * size) + size + leading;
+      }
+      // set the 'cursor' for the next char to be drawn, then loop again for next char
+      this.setCursor(offset, this.cursor_y);
+    }
+  }
+  if (immed) {
+    this._updateDirtyBytes(this.dirtyBytes);
+  }
+};
+
+// draw an individual character to the screen
+Oled.prototype._drawChar = function(font, byteArray, size, sync) {
+  // take your positions...
+  var x = this.cursor_x,
+      y = this.cursor_y;
+
+  var pagePos = 0;
+  var c = 0;
+  // loop through the byte array containing the hexes for the char
+  for (var i = 0; i < byteArray.length; i += 1) {
+    pagePos = Math.floor(i / font.width) * 8;
+    for (var j = 0; j < 8; j += 1) {
+      // pull color out
+      var color = byteArray[i][j],
+          xpos, ypos;
+      // standard font size
+      if (size === 1) {
+        xpos = x + c;
+        ypos = y + j + pagePos;
+        this.drawPixel([xpos, ypos, color], false);
+      } else {
+        // MATH! Calculating pixel size multiplier to primitively scale the font
+        xpos = x + (i * size);
+        ypos = y + (j * size);
+        this.fillRect(xpos, ypos, size, size, color, false);
+      }
+    }
+    c = (c < font.width -1) ? c += 1 : 0;
+  }
+};
+
+// get character bytes from the supplied font object in order to send to framebuffer
+Oled.prototype._readCharBytes = function(byteArray) {
+  var bitArr = [],
+      bitCharArr = [];
+  // loop through each byte supplied for a char
+  for (var i = 0; i < byteArray.length; i += 1) {
+    // set current byte
+    var byte = byteArray[i];
+    // read each byte
+    for (var j = 0; j < 8; j += 1) {
+      // shift bits right until all are read
+      var bit = byte >> j & 1;
+      bitArr.push(bit);
+    }
+    // push to array containing flattened bit sequence
+    bitCharArr.push(bitArr);
+    // clear bits for next byte
+    bitArr = [];
+  }
+  return bitCharArr;
+};
+
+// find where the character exists within the font object
+Oled.prototype._findCharBuf = function(font, c) {
+  // use the lookup array as a ref to find where the current char bytes start
+  var cBufPos = font.lookup.indexOf(c) * font.width;
+  // slice just the current char's bytes out of the fontData array and return
+  var cBuf = font.fontData.slice(cBufPos, cBufPos + font.width);
+  return cBuf;
+};
+
+// send the entire framebuffer to the oled
+Oled.prototype.update = function() {
+  // wait for oled to be ready
+  this._waitUntilReady(function() {
+    // set the start and endbyte locations for oled display update
+    var displaySeq = [
+      this.COLUMN_ADDR,
+      this.screenConfig.coloffset,
+      this.screenConfig.coloffset + this.WIDTH - 1, // column start and end address
+      this.PAGE_ADDR, 0, (this.HEIGHT / 8) - 1 // page start and end address
+    ];
+
+    var displaySeqLen = displaySeq.length,
+        bufferLen = this.buffer.length,
+        i, v;
+
+    // send intro seq
+    for (i = 0; i < displaySeqLen; i += 1) {
+      this._transfer('cmd', displaySeq[i]);
+    }
+
+    // write buffer data
+    this._transferData(this.buffer);
+
+  }.bind(this));
+
+  // now that all bytes are synced, reset dirty state
+  this.dirtyBytes = [];
+};
+
+// send dim display command to oled
+Oled.prototype.dimDisplay = function(bool) {
+  var contrast;
+
+  if (bool) {
+    contrast = 0; // Dimmed display
+  } else {
+    contrast = 0xCF; // Bright display
+  }
+
+  this._transfer('cmd', this.SET_CONTRAST);
+  this._transfer('cmd', contrast);
+};
+
+// turn oled off
+Oled.prototype.turnOffDisplay = function() {
+  this._transfer('cmd', this.DISPLAY_OFF);
+};
+
+// turn oled on
+Oled.prototype.turnOnDisplay = function() {
+  this._transfer('cmd', this.DISPLAY_ON);
+};
+
+// clear all pixels currently on the display
+Oled.prototype.clearDisplay = function(sync) {
+  var immed = (typeof sync === 'undefined') ? true : sync;
+  // write off pixels
+  //this.buffer.fill(0x00);
+  for (var i = 0; i < this.buffer.length; i += 1) {
+    if (this.buffer[i] !== 0x00) {
+      this.buffer[i] = 0x00;
+      if (this.dirtyBytes.indexOf(i) === -1) {
+        this.dirtyBytes.push(i);
+      }
+    }
+  }
+  if (immed) {
+    this._updateDirtyBytes(this.dirtyBytes);
+  }
+};
+
+// invert pixels on oled
+Oled.prototype.invertDisplay = function(bool) {
+  if (bool) {
+    this._transfer('cmd', this.INVERT_DISPLAY); // inverted
+  } else {
+    this._transfer('cmd', this.NORMAL_DISPLAY); // non inverted
+  }
+};
+
+// draw an image pixel array on the screen
+Oled.prototype.drawBitmap = function(pixels, width, sync) {
+  var immed = (typeof sync === 'undefined') ? true : sync;
+  var x, y,
+      pixelArray = [];
+
+  for (var i = 0; i < pixels.length; i++) {
+    x = i % width;
+    y = Math.floor(i / width);
+
+    this.drawPixel([x + this.cursor_x, y + this.cursor_y, pixels[i]], false);
+  }
+
+  if (immed) {
+    this._updateDirtyBytes(this.dirtyBytes);
+  }
+};
+
+// draw one or many pixels on oled
+Oled.prototype.drawPixel = function(pixels, sync) {
+  var immed = (typeof sync === 'undefined') ? true : sync;
+
+  // handle lazy single pixel case
+  if (typeof pixels[0] !== 'object') pixels = [pixels];
+
+  pixels.forEach(function(el) {
+    // return if the pixel is out of range
+    var x = el[0], y = el[1], color = el[2];
+    if (x > this.WIDTH || y > this.HEIGHT) return;
+
+    // thanks, Martin Richards.
+    // I wanna can this, this tool is for devs who get 0 indexes
+    //x -= 1; y -=1;
+    var byte = 0,
+        page = Math.floor(y / 8),
+        pageShift = 0x01 << (y - 8 * page);
+
+    // is the pixel on the first row of the page?
+    (page == 0) ? byte = x : byte = x + (this.WIDTH * page);
+
+    // colors! Well, monochrome.
+    if (color === 'BLACK' || color === 0) {
+      this.buffer[byte] &= ~pageShift;
+    }
+    if (color === 'WHITE' || color > 0) {
+      this.buffer[byte] |= pageShift;
+    }
+
+    // push byte to dirty if not already there
+    if (this.dirtyBytes.indexOf(byte) === -1) {
+      this.dirtyBytes.push(byte);
+    }
+
+  }, this);
+
+  if (immed) {
+    this._updateDirtyBytes(this.dirtyBytes);
+  }
+};
+
+// looks at dirty bytes, and sends the updated bytes to the display
+Oled.prototype._updateDirtyBytes = function(byteArray) {
+  var blen = byteArray.length, i,
+      displaySeq = [];
+
+  this._waitUntilReady(function() {
+	  var pageStart = Infinity, pageEnd = 0;
+    var colStart = Infinity, colEnd = 0, any = false;
+
+    // iterate through dirty bytes
+    for (var i = 0; i < blen; i += 1) {
+      var b = byteArray[i];
+      if ((b >= 0) && (b < this.buffer.length)) {
+        var page = b / this.WIDTH | 0;
+        if (page < pageStart) pageStart = page;
+        if (page > pageEnd) pageEnd = page;
+        var col = b % this.WIDTH;
+        if (col < colStart) colStart = col;
+        if (col > colEnd) colEnd = col;
+        any = true;
+      }
+    }
+
+    if (!any) return;
+
+    displaySeq = [
+      this.COLUMN_ADDR, colStart, colEnd, // column start and end address
+      this.PAGE_ADDR, pageStart, pageEnd // page start and end address
+    ];
+
+    var displaySeqLen = displaySeq.length, v, vp, vc;
+
+    // send intro seq
+    for (v = 0; v < displaySeqLen; v += 1) {
+      this._transfer('cmd', displaySeq[v]);
+    }
+
+    var bytes = [];
+    var idx = 0;
+    // send byte, then move on to next byte
+    for (vp = pageStart; vp <= pageEnd; vp += 1) {
+      for (vc = colStart; vc <= colEnd; vc += 1) {
+        bytes[idx] = this.buffer[this.WIDTH * vp + vc];
+        idx++;
+      }
+    }
+
+    this._transferData(bytes);
+
+  }.bind(this));
+
+  // now that all bytes are synced, reset dirty state
+  this.dirtyBytes = [];
+};
+
+// using Bresenham's line algorithm
+Oled.prototype.drawLine = function(x0, y0, x1, y1, color, sync) {
+  var immed = (typeof sync === 'undefined') ? true : sync;
+
+  var dx = Math.abs(x1 - x0), sx = x0 < x1 ? 1 : -1,
+      dy = Math.abs(y1 - y0), sy = y0 < y1 ? 1 : -1,
+      err = (dx > dy ? dx : -dy) / 2;
+
+  while (true) {
+    this.drawPixel([x0, y0, color], false);
+
+    if (x0 === x1 && y0 === y1) break;
+
+    var e2 = err;
+
+    if (e2 > -dx) {err -= dy; x0 += sx;}
+    if (e2 < dy) {err += dx; y0 += sy;}
+  }
+
+  if (immed) {
+    this._updateDirtyBytes(this.dirtyBytes);
+  }
+}
+
+// Draw an outlined  rectangle
+Oled.prototype.drawRect = function(x, y, w, h, color, sync){
+  var immed = (typeof sync === 'undefined') ? true : sync;
+  //top
+  this.drawLine(x, y, x + w, y,color,false);
+
+  //left
+  this.drawLine(x, y + 1, x, y + h - 1, color, false);
+
+  //right
+  this.drawLine(x + w, y + 1, x + w, y + h - 1, color, false);
+
+  //bottom
+  this.drawLine(x, y + h - 1, x + w, y + h - 1, color, false);
+
+  if (immed) {
+    this._updateDirtyBytes(this.dirtyBytes);
+  }
+};
+
+// draw a filled rectangle on the oled
+Oled.prototype.fillRect = function(x, y, w, h, color, sync) {
+  var immed = (typeof sync === 'undefined') ? true : sync;
+  // one iteration for each column of the rectangle
+  for (var i = x; i < x + w; i += 1) {
+    // draws a vert line
+    this.drawLine(i, y, i, y+h-1, color, false);
+  }
+  if (immed) {
+    this._updateDirtyBytes(this.dirtyBytes);
+  }
+};
+
+/**
+ * Draw a circle outline
+ *
+ * This method is ad verbatim translation from the corresponding
+ * method on the Adafruit GFX library
+ * https://github.com/adafruit/Adafruit-GFX-Library
+ */
+Oled.prototype.drawCircle = function(x0, y0, r, color, sync) {
+	var immed = (typeof sync === 'undefined') ? true : sync;
+
+	var f = 1 - r;
+	var ddF_x = 1;
+	var ddF_y = -2 * r;
+	var x = 0;
+	var y = r;
+
+	this.drawPixel(
+		[[x0, y0 + r, color],
+			[x0, y0 - r, color],
+			[x0 + r, y0, color],
+			[x0 - r, y0, color]],
+		false
+	);
+
+	while(x < y) {
+		if (f >=0) {
+			y--;
+			ddF_y += 2;
+			f += ddF_y;
+		}
+		x++;
+		ddF_x += 2;
+		f += ddF_x;
+
+		this.drawPixel(
+			[[x0 + x, y0 + y, color],
+				[x0 - x, y0 + y, color],
+				[x0 + x, y0 - y, color],
+				[x0 - x, y0 - y, color],
+				[x0 + y, y0 + x, color],
+				[x0 - y, y0 + x, color],
+				[x0 + y, y0 - x, color],
+				[x0 - y, y0 - x, color]],
+			false
+		);
+	}
+
+	if (immed) {
+		this._updateDirtyBytes(this.dirtyBytes);
+	}
+};
+
+// activate scrolling for rows start through stop
+Oled.prototype.startScroll = function(dir, start, stop) {
+  var scrollHeader,
+      cmdSeq = [];
+
+  switch (dir) {
+    case 'right':
+      cmdSeq.push(this.RIGHT_HORIZONTAL_SCROLL); break;
+    case 'left':
+      cmdSeq.push(this.LEFT_HORIZONTAL_SCROLL); break;
+    case 'left diagonal':
+      cmdSeq.push(
+        this.SET_VERTICAL_SCROLL_AREA,
+        0x00,
+        this.HEIGHT,
+        this.VERTICAL_AND_LEFT_HORIZONTAL_SCROLL,
+        0x00,
+        start,
+        0x00,
+        stop,
+        0x01,
+        this.ACTIVATE_SCROLL
+      );
+      break;
+    case 'right diagonal':
+      cmdSeq.push(
+        this.SET_VERTICAL_SCROLL_AREA,
+        0x00,
+        this.HEIGHT,
+        this.VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL,
+        0x00,
+        start,
+        0x00,
+        stop,
+        0x01,
+        this.ACTIVATE_SCROLL
+      );
+      break;
+  }
+
+  this._waitUntilReady(function() {
+    if(dir === 'right' || dir === 'left'){
+      cmdSeq.push(
+        0x00, start,
+        0x00, stop,
+        0x00, 0xFF,
+        this.ACTIVATE_SCROLL
+      );
+    }
+
+    var i, cmdSeqLen = cmdSeq.length;
+
+    for (i = 0; i < cmdSeqLen; i += 1) {
+      this._transfer('cmd', cmdSeq[i]);
+    }
+  }.bind(this));
+};
+
+// stop scrolling display contents
+Oled.prototype.stopScroll = function() {
+  this._transfer('cmd', this.DEACTIVATE_SCROLL); // stahp
+};
+
+module.exports = Oled;
---
+++ src/scripts/run.js
@@ -0,0 +1,363 @@
+require('dotenv').config()
+const OLED = require('../libs/oled')
+const font = require('oled-font-5x7')
+const pngparse = require('pngparse')
+const si = require('systeminformation')
+const moment = require('moment')
+const Twitter = require('twitter')
+
+/**
+ * initialize oled screen
+ */
+const initOLED = () => {
+  const options = {
+    width:   process.env.OLED_WIDTH,
+    height:  process.env.OLED_HEIGHT,
+    address: Number(process.env.OLED_ADDRESS),
+    device:  process.env.OLED_DEVICE
+  }
+
+  const oled = new OLED(options)
+  oled.dimDisplay(true)
+  oled.update()
+
+  return oled
+}
+
+/**
+ * clear oled screen
+ */
+const clearOLED = () => {
+  mData.intervalIDs.map(id => clearInterval(id))
+  mData.intervalIDs = []
+  mOled.clearDisplay()
+}
+
+/**
+ * allocate a random coordinate
+ */
+const allocXY = (maxX, maxY) => {
+  const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  const x = getRandomIntInclusive(0, maxX)
+  const y = getRandomIntInclusive(0, maxY)
+
+  return { x, y }
+}
+
+/**
+ * calculate element coordinate
+ */
+const calcElementXY = (x, y, height) => {
+  height = typeof height === 'number' ? height : font.height
+
+  const nx = x + mData.iconSize + mData.eleSpacing
+  const ny = y + Math.round((mData.iconSize - height) / 2)
+
+  return {
+    x: nx,
+    y: ny
+  }
+}
+
+/**
+ * calculate digit coordinate from right
+ */
+const calcElementDigitXY = (x, y, count = 2) => {
+  const nx = x + mData.width - (mData.digit.width * count + mData.digit.spacing)
+  const ny = y + Math.round((mData.iconSize - mData.digit.height) / 2)
+
+  return {
+    x: nx,
+    y: ny
+  }
+}
+
+/**
+ * fetch one twitter text
+ */
+const getTweet = (name = 'realDonaldTrump') => {
+  const client = new Twitter({
+    consumer_key:        process.env.TWITTER_API_KEY,
+    consumer_secret:     process.env.TWITTER_API_SECRET_KEY,
+    access_token_key:    process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+
+  const params = {
+    'screen_name': name,
+    'count': 10
+  }
+
+  return new Promise((resolve, reject) => {
+    client.get('statuses/user_timeline', params).then(data => {
+      let tweet = 'empty'
+      for (const item of data) {
+        if (item.text.startsWith('http')) continue
+        tweet = item.text + ' -- ' + item.user.name
+        break
+      }
+      resolve(tweet)
+    }).catch(err => {
+      console.log(0, err)
+      resolve('[err] ' + (err[0] ? err[0].message : err))
+    })
+  })
+}
+
+/**
+ * scroll text from right to left
+ * compatible for ssd1309
+ */
+const scrollTextLeft = (x, y, text, width = 0, delay = 50) => {
+  let i = 0
+  return setInterval(() => drawString(x, y, text, width, i--), delay)
+}
+
+/**
+ * draw string by bitmap
+ */
+const drawString = (x, y, text, width, position = 0) => {
+  // collect all bytes
+  let bytes = []
+  const chars = text.split('')
+  for (let i = 0; i < chars.length; i++) {
+    const charBuf = mOled._findCharBuf(font, chars[i])
+    const charBytes = mOled._readCharBytes(charBuf)
+    if (chars[i] === ' ') {
+      bytes = bytes.concat(charBytes)
+    } else {
+      const fillLength = charBytes[0] ? charBytes[0].length : 0
+      bytes = bytes.concat(charBytes, [Array(fillLength).fill(0)])
+    }
+  }
+
+  // scroll the bytes
+  if (position < 0) {
+    const byteParts = bytes.splice(0, -position % bytes.length)
+    bytes = bytes.concat(byteParts)
+  } else {
+    const byteParts = bytes.splice(-position % bytes.length)
+    bytes = byteParts.concat(bytes)
+  }
+
+  // resize the display width
+  const availableWidth = mOled.WIDTH - x
+  const targetWidth = width <= 0 || width > availableWidth ? availableWidth : width
+  bytes = bytes.splice(0, targetWidth)
+
+  // convert to bitmap
+  const bitmap = []
+  for (let i = 0; i < bytes[0].length; i++) {
+    for (let j = 0; j < bytes.length; j++) {
+      bitmap.push(bytes[j][i])
+    }
+  }
+
+  // draw bitmap
+  mOled.setCursor(x, y)
+  mOled.drawBitmap(Buffer.from(bitmap), bytes.length)
+}
+
+/**
+ * draw bitmap from asstes
+ */
+const drawBitmap = (x, y, file, dir = 'icons') => {
+  const filepath = __dirname + '/../assets/' + dir + '/' + file
+  return new Promise((resolve, reject) => {
+    pngparse.parseFile(filepath, (err, image) => {
+      if (err) return reject(err)
+      mOled.setCursor(x, y)
+      mOled.drawBitmap(image.data, image.width)
+      return resolve(image)
+    })
+  })
+}
+
+/**
+ * draw dot-intege-rnumber
+ */
+const drawDigit = (x, y, number) => {
+  const numbers = number.toString().split('')
+  for (const i in numbers) {
+    x = i == 0 ? x : x + mData.digit.spacing + mData.digit.width + 1
+    const filename = numbers[i] + '.png'
+    drawBitmap(x, y, filename, 'digits')
+  }
+}
+
+/**
+ * draw bar chart
+ */
+const drawBarChart = (x, y, w, h, percent, cpuMatrix = []) => {
+  const genColumn = percent => {
+    const data = []
+    const value = Math.ceil(percent * h)
+    for (let i = h - 1; i >= 0; i--) {
+      data.push(i >= value ? 0 : 1)
+    }
+    return data
+  }
+
+  if (cpuMatrix.length === 0) {
+    const lastRow = []
+    for (let i = 0; i < w; i++) {
+      lastRow.push(i % 3 ? 0 : 1)
+    }
+
+    for (let i = 0; i < h; i++) {
+      cpuMatrix[i] = i === h - 1 ? lastRow : Array(w).fill(0)
+    }
+  }
+
+  const bitmap = []
+  const column = genColumn(percent)
+  for (const i in cpuMatrix) {
+    const row = cpuMatrix[i]
+    row.shift()
+    row.push(column[i])
+    row.map(bit => bitmap.push(bit))
+  }
+
+  mOled.setCursor(x, y)
+  mOled.drawBitmap(Buffer.from(bitmap), w)
+}
+
+/**
+ * module - cpu usage
+ */
+const showCpuUsage = (x, y) => {
+  drawBitmap(x, y, 'cpu.png').then((image, err) => {
+    const matrix = []
+    const barWidth = 95
+    const barHeight = 10
+    mData.intervalIDs.push(setInterval(() => {
+      si.currentLoad().then(data => {
+        const load = Math.round(data.currentload)
+        const coord = calcElementXY(x, y, barHeight)
+        drawBarChart(coord.x, coord.y, barWidth, barHeight, load / 100, matrix)
+
+        const string = load < 10 ? '0' + load : load
+        const coord1 = calcElementDigitXY(x, y)
+        drawDigit(coord1.x, coord1.y, string)
+      })
+    }, 1000))
+  })
+}
+
+/**
+ * module - memory usage
+ */
+const showMemoryUsage = (x, y) => {
+  drawBitmap(x, y, 'memory.png').then((image, err) => {
+    const matrix = []
+    const barWidth = 95
+    const barHeight = 10
+    mData.intervalIDs.push(setInterval(() => {
+      si.mem().then(data => {
+        const usage = Math.round((data.total -data.available) / data.total * 100)
+        const coord = calcElementXY(x, y, barHeight)
+        drawBarChart(coord.x, coord.y, barWidth, barHeight, usage / 100, matrix)
+
+        const string = usage < 10 ? '0' + usage : usage
+        const coord1 = calcElementDigitXY(x, y)
+        drawDigit(coord1.x, coord1.y, string)
+      })
+
+    }, 1000))
+  })
+}
+
+/**
+ * module - system info
+ */
+const showSystemInfo = (x, y) => {
+  drawBitmap(x, y, 'dashboard.png').then((image, err) => {
+    mData.intervalIDs.push(setInterval(() => {
+      const time = si.time()
+      const uptime = moment(time.uptime * 1000).format('hh:mm:ss')
+
+      const coord = calcElementXY(x, y)
+      mOled.setCursor(coord.x, coord.y)
+      mOled.writeString(font, 1, uptime)
+
+      si.cpuCurrentspeed().then(data => {
+        mOled.setCursor(coord.x + 63, coord.y)
+        mOled.writeString(font, 1, data.avg.toString())
+      })
+
+      si.cpuTemperature().then(data => {
+        const coord = calcElementDigitXY(x, y)
+        drawDigit(coord.x, coord.y, data.main)
+      })
+    }, 1000))
+  })
+}
+
+/**
+ * module - network
+ */
+const showNetwork = (x, y) => {
+  drawBitmap(x, y, 'twitter.png').then((image, err) => {
+    let intervalId
+    const drawTweet = () => {
+      getTweet().then(tweet => {
+        clearInterval(intervalId)
+        const width = x + mData.width - mData.iconSize - mData.eleSpacing
+        const coord = calcElementXY(x, y)
+        intervalId = scrollTextLeft(coord.x, coord.y, tweet + ' ', width)
+        mData.intervalIDs.push(intervalId)
+      })
+    }
+
+    drawTweet()
+    mData.intervalIDs.push(setInterval(() => drawTweet(), 60000))
+  })
+}
+
+/**
+ * display modules
+ */
+const run = () => {
+  const spacingY = mData.iconSize + mData.modSpacing
+  const maxX = mOled.WIDTH - mData.width - 1
+  let maxY = mOled.HEIGHT - mData.height - 1
+  let { x, y } = allocXY(maxX, maxY)
+
+  showCpuUsage(x, y)
+  showMemoryUsage(x, y += spacingY)
+  showSystemInfo(x, y += spacingY)
+  showNetwork(x, y += spacingY)
+}
+
+// start
+const mData = {
+  width: 125,      // available width
+  height: 56,      // available height
+  iconSize: 12,    // icon width & height
+  eleSpacing: 4,   // element spacing
+  modSpacing: 3,   // module spacing
+  intervalIDs: [], // interval ids
+  digit: {         // dot number
+    width: 5,
+    height: 9,
+    spacing: 1
+  }
+}
+const mOled = initOLED()
+
+run()
+setInterval(() => {
+  clearOLED()
+  run()
+}, 600000)
+
+// clear display when catch an signal
+process.on('SIGINT', () => {
+  clearOLED()
+  setTimeout(() => process.exit(), 1000)
+})
